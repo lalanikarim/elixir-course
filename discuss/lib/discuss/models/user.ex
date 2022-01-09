59fe5c665fcbcd5cfcd80502b869aa770aedd873
Karim Lalani <jimmy00784@gmail.com>
2022-01-08 18:23:57 -0600

    added relationships and middleware

--- discuss/lib/discuss/models/user.ex
+++ discuss/lib/discuss/models/user.ex
@@ -8,6 +8,8 @@ defmodule Discuss.User do
     field :provider, :string
     field :token, :string
 
+    has_many :topics, Discuss.Topic
+
     timestamps()
   end
 
--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -4,6 +4,9 @@ defmodule DiscussWeb.TopicController do
   alias Discuss.Topic
   alias Discuss.Repo
 
+  plug DiscussWeb.Plugs.RequireAuth when action in [:new, :create, :edit, :update, :delete]
+  plug :check_topic_owner when action in [:edit, :update, :delete]
+
   def index(conn, _params) do
     render(conn, "index.html", topics: Repo.all(Topic))
   end
@@ -18,7 +21,9 @@ defmodule DiscussWeb.TopicController do
   end
 
   def create(conn, %{"topic" => topic}) do
-    changeset = Topic.changeset(%Topic{}, topic)
+    changeset = conn.assigns.user
+                |> Ecto.build_assoc(:topics)
+                |> Topic.changeset(topic)
 
     case Repo.insert(changeset) do
       {:ok, _topic} ->
@@ -28,6 +33,18 @@ defmodule DiscussWeb.TopicController do
 
       {:error, changeset} ->
         render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  def check_topic_owner(conn, _params) do
+    %{params: %{"id" => topic_id}} = conn
+    if Repo.get!(Topic, topic_id).user_id == conn.assigns.user.id do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You cannot edit that")
+      |> redirect(to: Routes.topic_path(conn,:index))
+      |> halt()
     end
   end
 
